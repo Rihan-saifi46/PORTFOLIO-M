9d4157cd2fad7da1bdf66a8dc6ba9d256802d51e
Rihan-saifi46 <kharihan786k@gmail.com>
2025-10-25 21:54:34 +0530

    add

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../sections/Hero'
+import About from '../sections/About'
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <About/>
     </div>
   )
 }
